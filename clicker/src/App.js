dd05dc7ac054f7b2ef3641777e13482907431f43
Thuong Ho <thuong.t.ho@gmail.com>
2020-08-27 12:18:40 -0700

    Add first renders without error test and convert App from functional component to class component

--- clicker/src/App.js
+++ clicker/src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    // data-test attribute so that everyone knows it is for testing and not to change it
+    return <div data-test='component-app'></div>;
+  }
 }
 
 export default App;
--- clicker/src/App.test.js
+++ clicker/src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+// Good basic test
+test('renders without an error', () => {
+  const wrapper = shallow(<App />);
+  // find using attribute selector
+  // naming convention is component-name-of-component
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toEqual(1);
 });
+
+// Core to the app
+test('renders increment button', () => {});
+
+test('renders counter display', () => {});
+
+// Test state
+test('counter starts at 0', () => {});
+
+// Test functionality
+test('clicking button increments counter display', () => {});
