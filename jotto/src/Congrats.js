752fc29356e89628c7e33941a186817dff0e2b0b
Thuong Ho <thuong.t.ho@gmail.com>
2020-09-28 12:13:34 -0700

    Add Congrats component and tests

---
+++ jotto/src/Congrats.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if `success` prop is false)
+ */
+export default (props) => {
+  if (props.success) {
+    return (
+      <div data-test='component-congrats'>
+        <span data-test='congrats-message'>
+          Congrats! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test='component-congrats' />;
+  }
+};
