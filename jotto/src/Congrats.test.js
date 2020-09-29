ffbd4fd66828a56e77dc959896595ccfbeac5ae4
Thuong Ho <thuong.t.ho@gmail.com>
2020-09-28 19:38:09 -0700

    Add propTypes for Congrats and use checkPropTypes in testing

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Congrats from './Congrats';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
@@ -18,7 +18,7 @@ const setup = (props = {}) => {
 };
 
 test('renders without an error', () => {
-  const wrapper = setup();
+  const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, 'component-congrats');
   expect(component.length).toBe(1);
 });
@@ -33,4 +33,9 @@ test('renders non-empty congrats message when `success` prop is true', () => {
   const wrapper = setup({ success: true });
   const component = findByTestAttr(wrapper, 'component-congrats');
   expect(component.text()).not.toBe(0);
+});
+
+test('does not throw warning with expected props', () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
--- jotto/test/testUtils.js
+++ jotto/test/testUtils.js
@@ -1,3 +1,4 @@
+import checkPropType from 'check-prop-types';
 /**
  * Return node(s) with the given data-test attribute.
  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
@@ -6,4 +7,20 @@
  */
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
+};
+
+/**
+ * Returns an assertion
+ * @param {FC.component} component - React component
+ * @param {object} conformingProps - props
+ */
+export const checkProps = (component, conformingProps) => {
+  // checkPropType returns an error if propTypes is not correct prop type
+  const propError = checkPropType(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name
+  );
+  expect(propError).toBeUndefined();
 };
