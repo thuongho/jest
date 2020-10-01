eddcca45725b0e02f430df27e414108810e8da06
Thuong Ho <thuong.t.ho@gmail.com>
2020-10-01 12:08:57 -0700

    Basic app component

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,22 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
 
-import Congrats from './Congrats';
-import GuessedWords from './GuessedWords';
-
 function App() {
-  const [success, setSuccess] = useState(true);
-  const [guessedWords, setGuessedWords] = useState([
-    { guessedWord: 'train', letterMatchCount: 1 }
-  ]);
-
-  return (
-    <div className='container'>
-      <h1>Jotto Game</h1>
-      <Congrats success={success} />
-      <GuessedWords guessedWords={guessedWords} />
-    </div>
-  );
+  return <div data-test='component-app'></div>;
 }
 
 export default App;
