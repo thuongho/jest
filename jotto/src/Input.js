64567587247fd8fa9fdd16d4f6b5c91f8297db33
Thuong Ho <thuong.t.ho@gmail.com>
2020-10-01 13:51:30 -0700

    Use propTypes in Input to define secretWord and add test

--- jotto/src/Input.js
+++ jotto/src/Input.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { PropTypes, string } from 'prop-types';
 
-const Input = (props) => {
+const Input = ({ secretWord }) => {
   return <div data-test='component-input'></div>;
+};
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired
 };
 
 export default Input;
--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 
 import Input from './Input';
 
+const defaultProps = {
+  secretWord: 'party'
+};
+
 /**
  * Setup function for Input component
+ * @param {object} props - Component props specific to this setup
  * @returns {ShallowWrapper}
  */
-const setup = () => {
-  return shallow(<Input />);
+const setup = (props) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Input {...setupProps} />);
 };
 
 test('renders Input component without error', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-input');
   expect(component.length).toBe(1);
+});
+
+test('does not throw warning with expected props', () => {
+  checkProps(Input, defaultProps);
 });
