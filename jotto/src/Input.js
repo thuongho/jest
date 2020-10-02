40d3055d6f943138b22d38ee176e4ece6f027ce0
Thuong Ho <thuong.t.ho@gmail.com>
2020-10-01 19:42:28 -0700

    Add Input local state currentGuess and tests

--- jotto/src/Input.js
+++ jotto/src/Input.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import { PropTypes, string } from 'prop-types';
+import { PropTypes } from 'prop-types';
 
 const Input = ({ secretWord }) => {
-  return <div data-test='component-input'></div>;
+  // Don't destructure and use React.useState for mocking
+  const [currentGuess, setCurrentGuess] = React.useState('');
+
+  return (
+    <div data-test='component-input'>
+      <form className='form-inline'>
+        <input
+          type='text'
+          data-test='input-box'
+          className='mb-2 mx-sm-3'
+          placeholder='enter guess'
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button
+          className='btn btn-primary mb-2'
+          data-test='submit-button'
+          onClick={(e) => {
+            e.preventDefault();
+            // TODO: update guessedWords
+            // TODO: check against secretWord and update success if needed
+            setCurrentGuess('');
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 Input.propTypes = {
--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -27,3 +27,33 @@ test('renders Input component without error', () => {
 test('does not throw warning with expected props', () => {
   checkProps(Input, defaultProps);
 });
+
+describe('state controlled input field', () => {
+  let mockSetCurrentGuess = jest.fn();
+  let wrapper;
+
+  beforeEach(() => {
+    // create a function to watch currentGuess
+    mockSetCurrentGuess.mockClear();
+    // replace useState of Input component and return our mock
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+    wrapper = setup();
+  });
+
+  test('state updates with value of input box upon change', () => {
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+    // simulate an input event
+    const mockEvent = { target: { value: 'train' } };
+    // apply even to change simulation on inputBox
+    inputBox.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+
+  test('field is cleared upon submit button click', () => {
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    submitButton.simulate('click', { preventDefault() {} });
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('');
+  });
+});
