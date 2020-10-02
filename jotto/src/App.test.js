b3a9a78b669448dbba967c05070d7c6357722c71
Thuong Ho <thuong.t.ho@gmail.com>
2020-10-01 23:35:27 -0700

    Update App component to use useReducer and useEffect to set the secretWord

--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
+import hookActions from './actions/hookActions';
 import App from './App';
+
+const mockGetSecretWord = jest.fn();
 
 /**
  * Setup function for app component.
- * @returns {ShallowWrapper}
+ * @returns {ReactWrapper}
  */
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+  // use mount cuz useEffect not called on shallow (enzyme issue2086)
+  return mount(<App />);
 };
 
 test('App renders without error', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-app');
   expect(component.length).toBe(1);
+});
+
+describe('getSecretWord calls', () => {
+  test('getSecretWord gets called on App mount', () => {
+    setup();
+
+    // check to see if secret word was updated
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
 });
